29bebd7d2ea3082d3f66bb8be46bd15703ea767f
fanek007 <82616117+fanek007@users.noreply.github.com>
2021-12-08 02:55:45 +0100

    Update server.go

--- speedtest/server.go
+++ speedtest/server.go
@@ -12,9 +12,12 @@ import (
 	"time"
 )
 
-// original URL "https://www.speedtest.net/speedtest-servers-static.php" did not return the nearest servers
-const speedTestServersUrl = "https://c.speedtest.net/speedtest-servers-static.php"
-const speedTestServersAlternativeUrl = "https://c.speedtest.net/speedtest-servers-static.php"
+// The original URL "https://www.speedtest.net/speedtest-servers-static.php" nor "https://c.speedtest.net/speedtest-servers-static.php" do not return the nearest servers.
+// This URL might be user instead: "https://www.speedtest.net/api/js/servers?engine=js&limit=10&https_functional=true".
+// More details here: https://issueexplorer.com/issue/sivel/speedtest-cli/765
+
+const speedTestServersUrl = "https://www.speedtest.net/speedtest-servers-static.php"
+const speedTestServersAlternativeUrl = "https://www.speedtest.net/speedtest-servers-static.php"
 
 // Server information
 type Server struct {
